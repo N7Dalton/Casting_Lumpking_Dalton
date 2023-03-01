980c6755aa8f125a04b281c7296946d1a8ef8f5a
Dalton <n7dalton@gmail.com>
2023-03-01 09:34:59 -0600

    update thing

--- Casting_Lumpking_Dalton/Program.cs
+++ Casting_Lumpking_Dalton/Program.cs
@@ -7,15 +7,29 @@ namespace Casting_Lumpking_Dalton
        
         static void Main(string[] args)
         {
-             
 
-        Console.WriteLine("whats your favorite Whole number?");
-           string favNum = Console.ReadLine();
+            //asks for fav number
+            Console.WriteLine("whats your favorite Whole number?");
+
+            //sets favNum to input
+            string favNum = Console.ReadLine();
+            
+            // asks if its true
             Console.WriteLine($"so your favorite number is {favNum} ?");
+
+            //finds them weird ig idk
             Console.WriteLine("nice....");
+
+            //asks about the best game of all time
             Console.WriteLine("do you like Atomic Heart?(true or false dumb dumb)");
+
+            //sets yesOrNo to input
             string yesOrNo = Console.ReadLine();
+
+            //writes line acting surprised either way
             Console.WriteLine($"No way! so its {yesOrNo} that you like Atomic Heart!");
+
+
 
 
         }
